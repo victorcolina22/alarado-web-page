68d1ab96f80b6e3d278ea12897907fb7d3a2a2a8
Victor Colina <victorcolina@G500606MAC272.local>
2024-01-05 00:04:50 -0300

    Page done, just left add dark/light mode functions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				floating: 'floating 5s linear infinite'
+			},
+			keyframes: {
+				floating: {
+					'0%': { transform: 'translateY(0)' },
+					'25%': { transform: 'translateY(20px)' },
+					'50%': { transform: 'translateY(0)' },
+					'70%': { transform: 'translateY(-20px)' },
+					'100%': { transform: 'translateY(0)' },
+				}
+			}
+		},
 	},
 	plugins: [],
 }
